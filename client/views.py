4de953da65ca106c1e052e4044b1c85a70576c0e
Adonis <avuglaado@gmail.com>
2025-03-05 13:07:15 +0000

    vs1

--- client/views.py
+++ client/views.py
@@ -8,6 +8,8 @@ class ScanQRView(View):
     def get(self, request):
         return render(request, 'client/scan_qr.html')
 
+
+
 class ProcessMixxQRView(View):
 
     def get(self, request):
@@ -19,12 +21,12 @@ class ProcessMixxQRView(View):
         return render(request, 'client/mixx_form.html', {
             'form': form,
             'agent_number': agent_number,
-            'transaction_code': None  # On initialise sans code
         })
 
     def post(self, request):
         agent_number = request.GET.get('agent_number', '')
         form = MixxClientForm(request.POST)
+        transaction_code = None  # Initialisation pour éviter les erreurs
 
         if form.is_valid():
             amount = form.cleaned_data['amount']
@@ -33,19 +35,15 @@ class ProcessMixxQRView(View):
             # Génération du code USSD
             transaction_code = f"*145*2*{amount}*{agent_number}*{personal_code}#"
 
-            # On renvoie directement le code pour qu'il s'affiche sur la page
-            return render(request, 'client/mixx_form.html', {
-                'form': form,
-                'agent_number': agent_number,
-                'transaction_code': transaction_code  # On passe le code à la page
-            })
-
         return render(request, 'client/mixx_form.html', {
             'form': form,
             'agent_number': agent_number,
-            'transaction_code': None
+            'transaction_code': transaction_code  # Toujours passer la variable
         })
-    
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import MoovClientForm
+
 class ProcessMoovQRView(View):
     def get(self, request):
         agent_number = request.GET.get('agent_number', '')
@@ -63,22 +61,16 @@ class ProcessMoovQRView(View):
     def post(self, request):
         agent_number = request.GET.get('agent_number', '')
         form = MoovClientForm(request.POST)
-        
+        transaction_code = None  # Initialisation pour éviter les erreurs
+
         if form.is_valid():
             secret_code = form.cleaned_data['secret_code']
-            
+
             # Construction du code de transaction
             transaction_code = f"*155*5*1*{secret_code}#"
-            
-            # On renvoie directement le code pour qu'il s'affiche sur la page
-            return render(request, 'client/moov_form.html', {
-                'form': form,
-                'agent_number': agent_number,
-                'transaction_code': transaction_code  # On passe le code à la page
-            })
-        
+
         return render(request, 'client/moov_form.html', {
             'form': form,
             'agent_number': agent_number,
-            'transaction_code': None
+            'transaction_code': transaction_code  # Toujours passer la variable
         })
